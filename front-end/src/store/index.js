2a7987006d02955b78b69f8107f92c14322a3e78
Aliaksei Vakhavich <alexei.c.asp.net@gmail.com>
2025-11-30 16:48:45 +0300

    front-end/src/store/index.js

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,14 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
 import cartReducer from './slices/cartSlice'
-import restaurantReducer from './slices/restaurantSlice'
 import orderReducer from './slices/orderSlice'
+import restaurantReducer from './slices/restaurantSlice'
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         cart: cartReducer,
+        orders: orderReducer,
         restaurants: restaurantReducer,
-        orders: orderReducer
-    }
+    },
 })
+
+export default store
